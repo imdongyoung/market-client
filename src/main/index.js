d165d2672a162433d8d97bebe569e6fbc7a8d7dd
imdongyoung <imdong207@gmail.com>
2022-01-12 22:54:57 +0900

    vercel

--- src/main/index.js
+++ src/main/index.js
@@ -2,14 +2,21 @@ import './index.css';
 import axios from 'axios';
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import 'dayjs/locale/ko';
+import { API_URL } from '../config/constants.js';
+import { Carousel } from 'antd';
+
+dayjs.extend(relativeTime);
+dayjs.locale('ko');
 
 function MainPage() {
   const [products, setProducts] = React.useState([]);
+  const [banners, setBanners] = React.useState([]);
   useEffect(() => {
     axios
-      .get(
-        'https://6ff360a8-b503-4795-8b6e-244eb1084e87.mock.pstmn.io/products'
-      )
+      .get(`${API_URL}/products`)
       .then((response) => {
         const products = response.data.products;
         setProducts(products);
@@ -17,30 +24,56 @@ function MainPage() {
       .catch((error) => {
         console.error('에러 발생 : ', error);
       });
+
+    axios
+      .get(`${API_URL}/banners`)
+      .then((result) => {
+        const banners = result.data.banners;
+        setBanners(banners);
+      })
+      .catch((error) => {
+        console.error('에러 발생 : ', error);
+      });
   }, []);
   return (
     <div>
-      <div id="banner">
-        <img src="./images/banners/banner1.png" alt="" />
-      </div>
-      <h1>판매되는 상품들</h1>
+      <Carousel autoplay autoplaySpeed={3000}>
+        {banners.map((banner, index) => {
+          return (
+            <Link to={banner.href}>
+              <div id="banner">
+                <img src={`${API_URL}/${banner.imageUrl}`} alt="" />
+              </div>
+            </Link>
+          );
+        })}
+      </Carousel>
+      <h1 id="product-headline">판매되는 상품들</h1>
       <div id="product-list">
         {products.map((product, index) => {
           return (
             <div className="product-card">
               <Link className="product-link" to={`products/${product.id}`}>
                 <div>
-                  <img className="product-img" src={product.imageUrl} />
+                  <img
+                    className="product-img"
+                    src={`${API_URL}/${product.imageUrl}`}
+                  />
                 </div>
                 <div className="product-contents">
                   <span className="product-name">{product.name}</span>
                   <span className="product-price">{product.price}원</span>
-                  <div className="product-seller">
-                    <img
-                      className="product-avatar"
-                      src="./images/icons/avatar.png"
-                    />
-                    <span>{product.seller}</span>
+                  <div className="product-footer">
+                    <div className="product-seller">
+                      <img
+                        className="product-avatar"
+                        src="./images/icons/avatar.png"
+                      />
+                      <span>{product.seller}</span>
+                    </div>
+                    <span className="product-date">
+                      {dayjs(product.createdAt).fromNow()}
+                    </span>
                   </div>
                 </div>
               </Link>
--- src/product/index.js
+++ src/product/index.js
@@ -2,17 +2,17 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import './index.css';
+import { API_URL } from '../config/constants.js';
+import dayjs from 'dayjs';
 
 function ProductPage() {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
   useEffect(() => {
     axios
-      .get(
-        `https://6ff360a8-b503-4795-8b6e-244eb1084e87.mock.pstmn.io/products/${id}`
-      )
-      .then((response) => {
-        setProduct(response.data);
+      .get(`${API_URL}/products/${id}`)
+      .then((result) => {
+        setProduct(result.data.product);
       })
       .catch((error) => {
         console.error(error);
@@ -26,7 +26,7 @@ function ProductPage() {
   return (
     <div>
       <div id="image-box">
-        <img src={'/' + product.imageUrl} />
+        <img src={`${API_URL}/${product.imageUrl}`} />
       </div>
       <div id="profile-box">
         <img src="/images/icons/avatar.png" />
@@ -35,8 +35,10 @@ function ProductPage() {
       <div id="contents-box">
         <div id="name">{product.name}</div>
         <div id="price">{product.price}</div>
-        <div id="createAt">2022년 1월 11일</div>
-        <div id="description">{product.description}</div>
+        <div id="createAt">
+          {dayjs(product.createdAt).format('YYYY년 MM월 DD일')}
+        </div>
+        <pre id="description">{product.description}</pre>
       </div>
     </div>
   );
